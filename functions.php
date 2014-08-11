93fed3835298819728b9c9d0d99b5153063e3d06
Charles J Dillon <charlesjdillon@ucadmins-MacBook-Pro.local>
2014-08-11 16:28:30 -0700

    Initial theme files for CMEC

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+// Global version tracker.
+
+include_once( 'includes/home-blog.php' ); // Include shortcode plugin.
+
+add_action( 'wp_enqueue_scripts', 'cleantech_enqueue_scripts' );
+/**
+ * Enqueue the script required by the Cleantech theme.
+ */
+function cleantech_enqueue_scripts() {
+	// Script only contains document.ready calls, load in the footer.
+	wp_enqueue_script( 'cleantech-script', get_stylesheet_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ), spine_get_script_version(), true );
+}
+
+add_action( 'spine_pre_jacket_html', 'cleantech_pre_jacket_html' );
+function cleantech_pre_jacket_html() {
+	echo '<div class="redTrim"></div>';
+}
