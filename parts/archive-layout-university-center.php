625f246e5c9835619f35381dd8e289343d16adad
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2015-09-22 11:09:36 -0700

    Alters the class to meet the headline html elements
    Alters the class to meet the headline html elements

--- parts/archive-layout-university-center.php
+++ parts/archive-layout-university-center.php
@@ -1,23 +1,16 @@
-<figure class="featured-image"></figure>
 <?php
-
-if ( is_post_type_archive( 'wsuwp_uc_entity' ) ) {
-	$archive_headline = 'Universities';
-} elseif ( is_post_type_archive( 'wsuwp_uc_person' ) ) {
-	$archive_headline = 'Members';
-} elseif ( is_post_type_archive( 'wsuwp_uc_project' ) ) {
-	$archive_headline = 'Projects';
-} elseif ( is_tax( 'wsuwp_uc_entity_type' ) ) {
-	$archive_headline = single_term_title( '', false );
+if ( is_post_type_archive( 'wsuwp_uc_person' ) ) {
+	$archive_headline = 'Personnel';
 } else {
 	$archive_headline = '';
 }
 ?>
+<header>
+	<span class="sub-header-default"><?php echo $archive_headline; ?></span>
+</header>
 <section class="row single gutter pad-ends entity-section">
 	<div class="column one">
-		<header>
-			<h1><?php echo $archive_headline; ?></h1>
-		</header>
+	
 
 		<?php while ( have_posts() ) : the_post(); ?>
 			<?php get_template_part( 'articles/post', get_post_type() ); ?>
